3d03beee78eb12776c8c7e5ae8a4db5d17466d03
Arthur <baginian.artur@yandex.ru>
2024-10-26 15:19:07 +0300

    Add parsing

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-// import (
-// 	"github.com/HayKor/goCalculator/pkg/calculator"
-// )
+import (
+	"fmt"
+
+	. "github.com/HayKor/goCalculator/pkg/parsing"
+)
 
 func main() {
+	input := "5 + 3 * (2 - 1)"
+	parser := NewParser(NewLexer(input))
+	result, err := parser.ParseExpression()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result) // Output: 8
 }
