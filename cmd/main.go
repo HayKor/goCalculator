99f833c6f9992f34f22dba9def0656723779751d
Arthur <baginian.artur@yandex.ru>
2024-10-26 15:26:46 +0300

    Use pkg/calculator

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 
-	. "github.com/HayKor/goCalculator/pkg/parsing"
+	. "github.com/HayKor/goCalculator/pkg/calculator"
 )
 
 func main() {
 	input := "5 + 3 * (2 - 1)"
-	parser := NewParser(NewLexer(input))
-	result, err := parser.ParseExpression()
+	result, err := Calc(input)
 	if err != nil {
 		fmt.Println(err)
 		return
--- pkg/calculator/calculator.go
+++ pkg/calculator/calculator.go
@@ -1,3 +1,21 @@
 package calculator
 
-func Calc(expression string) (float64, error) {}
+import (
+	. "github.com/HayKor/goCalculator/internal/parsing"
+)
+
+// Реализовать функцию func Calc(expression string) (float64, error)
+// expression - строка-выражение состоящее из односимвольных идентификаторов и знаков арифметических действий
+// Входящие данные - цифры(рациональные), операции +, -, *, /,
+// операции приоритезации ( и ) В случае ошибки записи выражения функция выдает ошибку.
+
+func Calc(expression string) (float64, error) {
+	lexer := NewLexer(expression)
+	parser := NewParser(lexer)
+
+	result, err := parser.ParseExpression()
+	if err != nil {
+		return 0, err
+	}
+	return result, nil
+}
