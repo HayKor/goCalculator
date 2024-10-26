810540803ddfecb5daf59e2e663e44c928edbec0
Arthur <baginian.artur@yandex.ru>
2024-10-26 16:32:43 +0300

    Improve tests

--- internal/parsing/parser_test.go
+++ internal/parsing/parser_test.go
@@ -8,21 +8,29 @@ func TestParser(t *testing.T) {
 	tests := []struct {
 		input       string
 		expectValue float64
+		wantErr     bool
 	}{
-		{"5+5", 10},
-		{"5+5*2", 15},
-		{"(5+5)*2", 20},
-		{"(5+5)*2+2", 22},
+		{"5+5", 10, false},
+		{"5+5*2", 15, false},
+		{"(5+5)*2", 20, false},
+		{"(5+5)*2+2", 22, false},
+
+		{"(5+5*2+2", 0, true},
+		{"5+5*2)", 0, true},
+		{"5+5*2))", 0, true},
+		{"(5+5*2))", 0, true},
 	}
 
 	for _, tc := range tests {
 		lexer := NewLexer(tc.input)
 		parser := NewParser(lexer)
 		gotValue, err := parser.ParseExpression()
-		if err != nil {
+		if err != nil && !tc.wantErr {
 			t.Errorf("Got error %v", err.Error())
 		}
-		if tc.expectValue != gotValue {
+		if err == nil && tc.wantErr {
+			t.Errorf("Expected error, got nil in %v", tc.input)
+		} else if tc.expectValue != gotValue {
 			t.Errorf("Expected %v, got %v", tc.expectValue, gotValue)
 		}
 	}
