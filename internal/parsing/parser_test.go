ab296a0e81887ff85f47b3857d1363f24f99efc1
Arthur <baginian.artur@yandex.ru>
2024-10-26 15:46:31 +0300

    Add tests for parser

--- internal/parsing/parser_test.go
+++ internal/parsing/parser_test.go
@@ -1,1 +1,29 @@
 package parsing
+
+import (
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	tests := []struct {
+		input       string
+		expectValue float64
+	}{
+		{"5+5", 10},
+		{"5+5*2", 15},
+		{"(5+5)*2", 20},
+		{"(5+5)*2+2", 22},
+	}
+
+	for _, tc := range tests {
+		lexer := NewLexer(tc.input)
+		parser := NewParser(lexer)
+		gotValue, err := parser.ParseExpression()
+		if err != nil {
+			t.Errorf("Got error %v", err.Error())
+		}
+		if tc.expectValue != gotValue {
+			t.Errorf("Expected %v, got %v", tc.expectValue, gotValue)
+		}
+	}
+}
