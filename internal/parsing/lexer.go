b427d27b97d99e22ab748df1ba351df1bc217fdb
Arthur <baginian.artur@yandex.ru>
2024-10-27 12:05:03 +0300

    Move `Token` to separate file

--- internal/parsing/lexer.go
+++ internal/parsing/lexer.go
@@ -4,24 +4,6 @@ import (
 	"unicode"
 )
 
-type TokenType int
-
-const (
-	Number TokenType = iota
-	Plus
-	Minus
-	Multiply
-	Divide
-	EOF
-	LeftParen
-	RightParen
-)
-
-type Token struct {
-	Type  TokenType
-	Value string
-}
-
 type Lexer struct {
 	input string
 	pos   int
